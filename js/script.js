4b224f0bf3a7003bfb581a71794f11416bd78cac
Mayank Jain <53962453+mayank-jain-wd@users.noreply.github.com>
2021-08-20 14:05:39 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  // numVisible: 3
+  indicators: true
+});
+$('.materialboxed').materialbox();
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true
+});
+$(".sidenav").sidenav();
+});
+
